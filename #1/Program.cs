9ea90a7be1a4110702caf72591e9fbbe5ffe8591
Centurion195 <Vadim-0211@yandex.ru>
2023-02-24 18:15:06 +0300

    Task #1 completed

---
+++ #1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void NaturalNumbers(int M, int N){
+    Console.Write($"{M}\t");
+    if (M>N) NaturalNumbers(M-1, N);
+    else if (N>M) NaturalNumbers(M+1, N);
+}
+
+int ReadInt(string message){
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int M = ReadInt("Введите число М:");
+int N = ReadInt("Введите число N:");
+NaturalNumbers(M, N);
