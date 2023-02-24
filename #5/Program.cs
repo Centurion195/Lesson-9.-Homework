f15c240738ae5bd4ebbbad90b3c36f50e54b254c
Centurion195 <Vadim-0211@yandex.ru>
2023-02-25 00:01:23 +0300

    Task #5 completed

--- #5/Program.cs
+++ #5/Program.cs
@@ -1,17 +1,21 @@
 ﻿// Написать программу возведения числа А в целую стень B
 
-int Exponentiation(int a, int b){
-    if (b>0) return a * Exponentiation(a, b-1);
-    // if (b<0) return 1 / a * Exponentiation(a, b+1);
-    if(b==0) return 1;
+int Exponentiation(int a, int b)
+{
+    if (b > 0) return a * Exponentiation(a, b - 1);
+    if (b < 0) return a * Exponentiation(a, b + 1);
+    if (b == 0) return 1;
     return 1;
 }
 
-int ReadInt(string message){
+int ReadInt(string message)
+{
     Console.WriteLine(message);
     return int.Parse(Console.ReadLine()!);
 }
 
 int a = ReadInt("Введите число A:");
 int b = ReadInt("Введите число B:");
-Console.WriteLine(Exponentiation(a, b));
+double result = Convert.ToDouble(Exponentiation(a, b));
+if (b < 0) result = Math.Round(1 / result, 3);
+Console.WriteLine($"Результат: {result}");
