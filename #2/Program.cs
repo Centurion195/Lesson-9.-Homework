74aa19a1b6e2dbef0a89cb82fe7755806662bd11
Centurion195 <Vadim-0211@yandex.ru>
2023-02-24 18:25:27 +0300

    Task #2 completed

---
+++ #2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumNumbers(int M, int N){
+    if (M>N) return M + SumNumbers(M-1, N);
+    else if (N>M) return M + SumNumbers(M+1, N);
+    return M;
+}
+
+int ReadInt(string message){
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int M = ReadInt("Введите число М:");
+int N = ReadInt("Введите число N:");
+Console.WriteLine(SumNumbers(M, N));
