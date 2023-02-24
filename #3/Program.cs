297b1792b80812abdec9c757f48bb2605f230a67
Centurion195 <Vadim-0211@yandex.ru>
2023-02-24 18:26:32 +0300

    Task #3 completed

---
+++ #3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int SumNumbers(int number)
+{
+    if (number == 0) return 0;
+    return SumNumbers(number/10) + number % 10; //5+2+1
+}
+
+Console.Write("Введите N: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNumbers(number));
